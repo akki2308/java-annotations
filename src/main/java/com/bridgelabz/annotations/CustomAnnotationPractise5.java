da2668bb2c8bf9c3fd3f516f03f3c297f9d2e4b5
Akki <chauhanakshit61@gmail.com>
2025-04-05 11:17:11 +0530

    [Akshit] ADD: Creating a Custom Annotaion to restrict access based on role

---
+++ src/main/java/com/bridgelabz/annotations/CustomAnnotationPractise5.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.annotations;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface RoleAllowed {
+    String value();
+}
+
+class SecurityContext {
+    static String currentUserRole = "USER";
+}
+
+@RoleAllowed("ADMIN")
+class AdminService {
+    void performAdminTask() {
+        System.out.println("Admin task performed!");
+    }
+}
+
+public class CustomAnnotationPractise5 {
+    public static void main(String[] args) {
+        Class<AdminService> cls = AdminService.class;
+
+        if (cls.isAnnotationPresent(RoleAllowed.class)) {
+            String allowed = cls.getAnnotation(RoleAllowed.class).value();
+            String current = SecurityContext.currentUserRole;
+
+            if (allowed.equals(current)) {
+                new AdminService().performAdminTask();
+            } else {
+                System.out.println("Access Denied!");
+            }
+        }
+    }
+}
